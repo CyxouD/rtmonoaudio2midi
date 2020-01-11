1ae14fada4de98783e5ac057be00f5d8f57696a8
Dima Zasukha <dima@stormotion.io>
2020-01-11 19:53:11 +0200

    feat: pass filename

--- tabulature.py
+++ tabulature.py
@@ -11,7 +11,7 @@ from audiostream import StreamProcessor
 # LilyPond app should be installed
 class Tabulature:
     @staticmethod
-    def show(notation, fundamental_frequencies_infos):
+    def show(notation, fundamental_frequencies_infos, filename):
         print('fundamental_frequencies_infos', fundamental_frequencies_infos)
         print('len(fundamental_frequencies_infos)', len(fundamental_frequencies_infos))
         track = Track()
@@ -25,11 +25,12 @@ class Tabulature:
         print('track', track)
         lyString = ("\\new TabStaff " if notation == 'tablature' else "") + LilyPond.from_Track(track)
         print('lyString', lyString)
-        LilyPond.to_pdf(lyString, "lilypond_generated/my_first_bar")
+        LilyPond.to_pdf(lyString, "lilypond_generated/%s" % filename)
 
 
 if __name__ == '__main__':
+    filename = "AR_Lick1_KN.wav"
     result = StreamProcessor(
-        "./test_data/IDMT-SMT-GUITAR_V2/dataset2/audio/AR_Lick1_KN.wav",
+        "./test_data/IDMT-SMT-GUITAR_V2/dataset2/audio/%s" % filename,
         bits_per_sample=24).run()
-    Tabulature.show('tablature', result.fundamental_frequencies_infos)
+    Tabulature.show('tablature', result.fundamental_frequencies_infos, filename)
